9611b3ae9a587b913a62b1ed8ed00b72b7f414a4
Akshay Pratap Singh <akshaypsingh333.devac@gmail.com>
2024-06-20 14:05:22 +0530

    const,var...

---
+++ Basics1/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 333111
+let accountEmail = "akshay@google.com"
+var accountpassword = "12345"
+accountCity = "Lucknow"
+
+console.log(accountId);
+// accountId = 2// not allowed cuz it is constant
+accountEmail = "akshaypratap@google.com"
+accountpassword = "11111"
+accountCity = "Noida"
+console.table([accountEmail,accountpassword,accountCity,accountId])
